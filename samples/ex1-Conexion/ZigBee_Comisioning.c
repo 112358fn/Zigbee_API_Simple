b28f779f5efa4a1eabd74aec4db6fe3494cd0f6a
Alvaro Alonso <112358.fn@gmail.com>
2014-12-01 19:26:33 +0000

    Clean code of Zigbee_Commissioning

--- samples/ex1-Conexion/ZigBee_Comisioning.c
+++ samples/ex1-Conexion/ZigBee_Comisioning.c
@@ -128,134 +128,19 @@ int main(int argc, char **argv)
 void
 handshake(zigbee* zb_elem, int serialFd){
 	printf("New Zigbee conexion\n");
+
 	//---- Generate the API Frame for Transmit request
 	unsigned char RFdata[5] = {'h','o','l','a','\0'};
 	unsigned char * API_frame= ZBTR_request(zb_elem->address, zb_elem->network, 0, 0, RFdata, 5);
+
 	//---- Send API frame
 	if(API_frame==NULL)return;
 	int n= API_frame_length(API_frame);
 	if((n) != write(serialFd,API_frame,n))
 		printf("Fail to send APIframe to serial\n");
 
-	//---- show the API FRAME
-	printf("API Frame: ");
-	int length=ZBTR_request_length(sizeof(RFdata));
-	for(int i=0;i<length; i++)printf("%02x:",API_frame[i]);
-	printf("\n");
-
-
 	//---- Free Memory Space
 	free(API_frame);
 	return;
 }
-
-void
-test_ZBTR_status(data_frame * data){
-	//---- Welcome Message
-	printf("Zigbee Transmit Status\n");
-	//---- Declare 16 bit Address
-	unsigned char address[2];
-	get_ZBTR_status_address16(data, address);
-	printf("* 16-bit Address:%02x%02x\n",address[0],address[1]);
-	//---- Number of transmission retries
-	unsigned char retrycount=\
-	get_ZBTR_status_retrycount(data);
-	printf("* Transmit retry count: %d\n",retrycount);
-	//---- Delivery Status
-	printf("* Delivery Status: ");
-	switch\
-	(get_ZBTR_status_deliveryST(data)){
-		case SUCCESS: printf("Success\n");
-			break;
-		default: printf("Delivery Error\n");
-			break;
-	}
-	//---- Discovery Status
-	printf("* Discovery Status: ");
-	switch\
-	(get_ZBTR_status_discoveryST(data)){
-		case NO_OHEAD:printf("No Discovery Overhead\n");
-			break;
-		case ADDR_DISC:printf("Address Discovery\n");
-			break;
-		case ROUTE_DISC:printf("Route Discovery\n");
-			break;
-		case ADDR_ROUTE:printf("Address and Route\n");
-			break;
-		case TIMEOUT:printf("Extended Timeout Discovery\n");
-			break;
-	}
-	return;
-}
-void
-test_NODE_id(data_frame * data){
-	//---- Welcome Message ----
-	printf("Node Identification Indicator\n");
-	//---- Decode Node Identification Indicator
-		//---- Zigbee Frame ----
-		zigbee* zb_elem = NODE_id_decode(data);
-		//----64-bit Source Address
-		printf("* 64-bit Address: ");
-		for(int i=0; i<8; i++)printf("%02x:",zb_elem->address[i]);
-		printf("\n");
-		//---- 16-bit Source Network Address
-		printf("* 16-bit Address: ");
-		printf("%02x:%02x\n",zb_elem->network[0],zb_elem->network[1]);
-		//---- String Node Identifier
-		printf("* String: %s\n",zb_elem->string);
-		//---- Parent Address
-		printf("* 16-bit Parent Address: ");
-		printf("%02x:%02x\n",zb_elem->parent[0],zb_elem->parent[1]);
-		//---- Device Type
-		printf("* Device type: ");
-		switch(zb_elem->devicetype){
-			case COORD:printf("Coordinator\n");
-				break;
-			case ROUTER:printf("Router\n");
-				break;
-			case END_DEV:printf("End Device\n");
-				break;
-			default:printf("No option\n");
-				break;
-		}
-
-	//----64-bit Source Address
-	printf("* 64-bit Source Address: ");
-	unsigned char address64[8];
-	get_NODE_id_source_addr64(data, address64);
-	for(int i=0; i<8; i++)printf("%02x:",address64[i]);
-	printf("\n");
-	//---- 16-bit Source Network Address
-	printf("* 16-bit Source Address: ");
-	unsigned char address16[2];
-	get_NODE_id_source_addr16(data, address16);
-	printf("%02x:%02x\n",address16[0],address16[1]);
-	//---- Receive options
-	printf("* Options: ");
-	switch(get_NODE_id_options(data)){
-		case PKT_ACK:printf("Packet Acknowledged\n");
-			break;
-		case PKT_BRD:printf("Packet was a broadcast packet\n");
-			break;
-		default:printf("No option\n");
-			break;
-	}
-	//---- Receive Events
-	printf("* Event: ");
-	switch(get_NODE_id_event(data)){
-		case BUTTON:printf("Frame sent by node identification pushbutton event\n");
-			break;
-		case JOIN_EV:printf("Frame sent after joining event occurred\n");
-			break;
-		case POWER_EV:printf("Frame sent after power cycle event occurred\n");
-			break;
-		default:printf("No option\n");
-			break;
-	}
-	//.... Free memory
-	free(zb_elem);
-	return;
-}
-
-
 //----------------------------------------------------------------------------
